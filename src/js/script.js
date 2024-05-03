5d6f537deecc586ff6facb971ad284b3fcce6d16
kavindu0818 <kavindumaduranga184@gmail.com>
2024-05-03 23:27:10 +0530

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,24 @@
-
-$("#customer").hide(); // Show Customer section
-$("#item").hide();     // Hide Item section
+$("#customer").hide();
+$("#item").hide();
 $("#orders").hide();
+
 $("#customer-btn").on('click', () => {
-    $("#customer").show(); // Show Customer section
-    $("#home").hide();     // Hide Home section
-    $("#item").hide();     // Hide Item section
-    $("#orders").hide();   // Hide Orders section
+    $("#customer").show();
+    $("#home").hide();
+    $("#item").hide();
+    $("#orders").hide();
 });
 
 $("#item-btn").on('click', () => {
-    $("#item").show();     // Show Item section
-    $("#home").hide();     // Hide Home section
-    $("#customer").hide(); // Hide Customer section
-    $("#orders").hide();   // Hide Orders section
+    $("#item").show();
+    $("#home").hide();
+    $("#customer").hide();
+    $("#orders").hide();
 });
 
-$("#Order-btn").on('click', () => { // Corrected ID to match HTML
-    $("#orders").show();    // Show Orders section
-    $("#home").hide();      // Hide Home section
-    $("#customer").hide();  // Hide Customer section
-    $("#item").hide();      // Hide Item section
+$("#Order-btn").on('click', () => {
+    $("#orders").show();
+    $("#home").hide();
+    $("#customer").hide();
+    $("#item").hide();
 });
