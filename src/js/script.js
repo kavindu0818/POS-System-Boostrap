6d8bc2b4b555540d5447a08810cf1011cdfd84cc
kavindu0818 <kavindumaduranga184@gmail.com>
2024-05-09 23:28:33 +0530

    development

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
+import {CustomerModel} from "../js/customerModel.js";
+import {customer} from "../js/db.js";
+
+
 $("#customer").hide();
 $("#item").hide();
 $("#orders").hide();
@@ -21,4 +25,29 @@ $("#Order-btn").on('click', () => {
     $("#home").hide();
     $("#customer").hide();
     $("#item").hide();
+});
+
+$(document).ready(function(){
+    $("#add-btn-cus").click(function(){
+        var inputValueId = $("#add-inp-cusId").val();
+        var inputValueFname = $("#add-inp-cusName").val();
+        var inputValueAddress = $("#add-inp-cusAddress").val();
+        var inputValueSalary = $("#add-inp-cusSalary").val();
+
+        var cusObj = new  CustomerModel(inputValueId, inputValueFname, inputValueAddress, inputValueSalary);
+
+        // Push the customer object into the customer array
+        customer.push(cusObj);
+
+        // Generate HTML markup for a new table row
+        var newRow = `<tr>
+            <td>${inputValueId}</td>
+            <td>${inputValueFname}</td>
+            <td>${inputValueAddress}</td>
+            <td>${inputValueSalary}</td>
+        </tr>`;
+
+        // Append the new row to the table
+        $("#cus-table tbody").append(newRow);
+    });
 });
